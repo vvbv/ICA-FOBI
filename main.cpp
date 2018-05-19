84e0ff7469d7739aad0bfc51066574f63c2de921
Jeison Cardona Gómez <jeison.cardona@correounivalle.edu.co>
2018-05-19 14:31:05 -0500

    + Matriz de covarianza.

--- main.cpp
+++ main.cpp
@@ -17,7 +17,7 @@ int main(){
     int numSamples_1 = audioFile_1.getNumSamplesPerChannel();
     int numSamples_2 = audioFile_2.getNumSamplesPerChannel();
 
-    arma::Mat <double>  data_matrix(2, numSamples_1);
+    arma::Mat <double>  data_matrix( numSamples_1, 2);
     
     for( int x = 0; x < numSamples_1; x++ ){
         data_matrix.col(0).row(x) = audioFile_1.samples[channel][x];
@@ -27,10 +27,8 @@ int main(){
         data_matrix.col(1).row(x) = audioFile_2.samples[channel][x];
     };
     
-    //arma::mat y = {{0,1},{2,3},{2,3},{2,3},{2,3},{2,3},{2,3}};
-    //arma::mat x = arma::cov( y );
-    arma::mat x = arma::cov( data_matrix );
-    std::cout << x << std::endl;
+    arma::mat covariance_matrix = arma::cov( data_matrix );
+   
 
     return 0;
 };
