9c84941655316ca747bf37f06a2c3c92d9f994e4
Jeison Cardona Gómez <jeison.cardona@correounivalle.edu.co>
2018-05-19 11:20:52 -0500

    + Lectura de .wav

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 rm build/ica_fobi
-g++ --std=c++11 main.cpp -o build/ica_fobi
+g++ --std=c++11 *.cpp -o build/ica_fobi
 ./build/ica_fobi
--- main.cpp
+++ main.cpp
@@ -1,10 +1,17 @@
 #include <armadillo>
 #include <iostream>
+#include "AudioFile.h"
+#include <string>
+
+std::string resource_forlder = "resources/";
 
 
 int main(){
 
-    arma::vec Pos = {{0},{1}};
-    Pos.print("Current position of the particle");
+    AudioFile<double> audioFile;
+    audioFile.load ( resource_forlder + "source1.wav" );
+    int sampleRate = audioFile.getSampleRate();
+    std::cout << std::to_string( sampleRate ) << std::endl;
+
     return 0;
 };
