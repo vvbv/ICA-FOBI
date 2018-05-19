f80116243fe6516a82e6e74b9529d919b72f42f8
Jeison Cardona Gómez <jeison.cardona@correounivalle.edu.co>
2018-05-19 11:04:27 -0500

    + Resource y prueba de armadillo.

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
-rm ica_fobi
-g++ --std=c++11 main.cpp -o ica_fobi
-ica_fobi
+rm build/ica_fobi
+g++ --std=c++11 main.cpp -o build/ica_fobi
+./build/ica_fobi
--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
-#include <iostrea>
+#include <armadillo>
+#include <iostream>
+
 
 int main(){
 
+    arma::vec Pos = {{0},{1}};
+    Pos.print("Current position of the particle");
     return 0;
 };
