e4353122ed7cdb3af9f514cc5dbab6c1d16f604d
emyesme <emily.carvajal@correounivalle.edu.co>
2018-05-22 15:32:05 -0500

    Update main.cpp
    segunda mitad de comentarios, duda

--- main.cpp
+++ main.cpp
@@ -64,37 +64,38 @@ int main(){
     arma::mat transpose_eigvectors_dot_data_matrix = data_matrix*transpose_eigvectors;
     //finaliza la primera etapa calculando la matriz blanqueada como un producto de matrices
     arma::mat whitened_data = transpose_eigvectors_dot_data_matrix*inverse_root_diagonal_eigvalues;
-
+    //SEGUNDA PARTE
     arma::vec arma_whitened_data_norm_2_axis_0;
     std::vector < double > whitened_data_norm_2_axis_0;
+    //se crea una matriz donde se guardaran por filas la norma dos de la matriz blanqueada
     for( int x = 0; x < whitened_data.col(0).n_elem; x++ ){
         whitened_data_norm_2_axis_0.push_back( arma::norm(whitened_data.row(x), 2) );
     };
 
     arma_whitened_data_norm_2_axis_0 = whitened_data_norm_2_axis_0;
-
     arma::mat matrix_norm( arma_whitened_data_norm_2_axis_0.n_elem, 2 );
-
+    //en matrix_norm guarda en todas sus columnas las filas de la matriz con la norma 2 de la matriz blanqueada
     for( int x = 0; x < arma_whitened_data_norm_2_axis_0.n_elem; x++ ){
         matrix_norm.row(x).col(0) = arma_whitened_data_norm_2_axis_0[x];
         matrix_norm.row(x).col(1) = arma_whitened_data_norm_2_axis_0[x];
     };
-
+	
     arma::mat m_nomr_multiply_wd_norm( matrix_norm.col(0).n_elem, 2 );
+    //multiplica cada norma de la matriz blanqueada con su correspondiente posicion en la matriz blanqueada
     for( int x = 0; x < matrix_norm.col(0).n_elem; x++ ){
         m_nomr_multiply_wd_norm.row(x).col(0) = matrix_norm.row(x).col(0) * whitened_data.row(x).col(0);
         m_nomr_multiply_wd_norm.row(x).col(1) = matrix_norm.row(x).col(1) * whitened_data.row(x).col(1);
     };
-
-    arma::mat covariance_m_nomr_multiply_wd_norm = arma::cov( m_nomr_multiply_wd_norm );
+    //se calculo la matriz de covarianza de matriz blanqueada por su norma
+    arma::mat covariance_m_nomr_multiply_wd_norm = arma::cov( m_nomr_multiply_wd_norm );//(*)
 
     arma::vec eigvalues_m_nomr_multiply_wd_norm;
     arma::mat eigvectors_m_nomr_multiply_wd_norm;
-
+    //se calcularon los eigenvalores y eigenvectores de la matriz de covarianza anterior(*)
     arma::eig_sym(eigvalues_m_nomr_multiply_wd_norm, eigvectors_m_nomr_multiply_wd_norm, covariance_m_nomr_multiply_wd_norm);
-
+    //calcula la matriz transpuesta de la matriz de eigenvectores de la matriz de covarianza anterior (*)
     arma::mat transposed_eigvectors_m_nomr_multiply_wd_norm = eigvectors_m_nomr_multiply_wd_norm.t();
-
+    //se hace el producto de matriz de eigenvectores con la matriz blanqueada transpuesta
     arma::mat source = eigvectors_m_nomr_multiply_wd_norm*whitened_data.t();
 
     std::cout << "\nSeparación terminada: Test \n\n" << source.col( 0 ) << std::endl << std::endl;
